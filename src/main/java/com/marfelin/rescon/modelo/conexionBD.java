7213d6900d13a84a82c29bcf184eadbed6680b56
Marfrlin <marfelin0102@gmail.com>
2025-07-27 18:35:55 -0400

    haciendo pruebas con la extencion SQLite para poblar la db

--- src/main/java/com/marfelin/rescon/modelo/conexionBD.java
+++ src/main/java/com/marfelin/rescon/modelo/conexionBD.java
@@ -1,7 +1,20 @@
 package com.marfelin.rescon.modelo;
 
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class conexionBD {
-    
+    private static final String URL = "jdbc:sqlite:db/rescon.db"; // Cambia el nombre si quieres otra base
+
+    public static Connection conectar() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(URL);
+            System.out.println("Conexión a SQLite establecida.");
+        } catch (SQLException e) {
+            System.out.println("Error al conectar a SQLite: " + e.getMessage());
+        }
+        return conn;
+    }
 }
